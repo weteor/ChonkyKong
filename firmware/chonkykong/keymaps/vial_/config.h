84f5301236daf91405cf4910578a6d34f6459da8
weteor <github@wetreg.fastmail.net>
2023-04-05 15:31:34 +0200

    add firmware

---
+++ firmware/chonkykong/keymaps/vial_/config.h
@@ -0,0 +1,25 @@
+/*
+Copyright 2022 weteor
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4B, 0x25, 0xFF, 0x63, 0xAE, 0x22, 0x5A, 0x5A}
+
+#define VIAL_KEY_OVERRIDE_ENTRIES 20
+#define VIAL_COMBO_ENTRIES 20
+#define VIAL_TAP_DANCE_ENTRIES 20
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
